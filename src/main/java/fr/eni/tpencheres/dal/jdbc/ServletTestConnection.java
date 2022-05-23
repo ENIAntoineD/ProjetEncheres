43f8af4304e60094423299779546ab88a9e91f72
antoi <antoine.david2022@campus-eni.fr>
2022-05-23 17:10:55 +0200

    Message

--- src/main/java/fr/eni/tpencheres/dal/jdbc/ServletTestConnection.java
+++ src/main/java/fr/eni/tpencheres/dal/jdbc/ServletTestConnection.java
@@ -26,7 +26,6 @@ public class ServletTestConnection extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
