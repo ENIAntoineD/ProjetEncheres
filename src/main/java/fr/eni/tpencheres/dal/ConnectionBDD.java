3180036306df798874e569ef623f89bea7eecf48
zarbio <beney.pierre21@gmail.com>
2022-05-23 16:21:37 +0200

    connexion bdd

--- src/main/java/fr/eni/tpencheres/dal/ConnectionBDD.java
+++
@@ -1,7 +0,0 @@
-package fr.eni.tpencheres.dal;
-
-public abstract class ConnectionBDD {
-	
-	private String test;
-
-}
---
+++ src/main/java/fr/eni/tpencheres/dal/jdbc/ConnectionBDD.java
@@ -0,0 +1,33 @@
+package fr.eni.tpencheres.dal.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public abstract class ConnectionBDD {
+	
+	private static DataSource ds = null;
+	static 
+	{
+		try {
+			Context cxt = new InitialContext();
+			 ds = (DataSource) cxt.lookup("java:comp/env/jdbc/sqlserver");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException
+	{
+	
+			return ds.getConnection();
+	
+	
+
+}
+	
+}
