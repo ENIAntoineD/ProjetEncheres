6723251fdf5fbf9b1b7eece7603cf580b0772d34
zarbio <beney.pierre21@gmail.com>
2022-05-31 17:00:29 +0200

    profil correctif

--- src/main/java/fr/eni/ProjetEncheres/ModifierProfil.java
+++ src/main/java/fr/eni/ProjetEncheres/ModifierProfil.java
@@ -45,8 +45,9 @@ public class ModifierProfil extends HttpServlet {
 		
 		if(request.getParameter("btEnregistrer") != null) {
 			user = userdao.getid(index);
-			user = new Utilisateur(index, 23, request.getParameter("pseudo"), request.getParameter("email"), request.getParameter("Nom"),request.getParameter("Prenom"),
-					request.getParameter("Adresse"), request.getParameter("Cp"), request.getParameter("Ville"), request.getParameter("Telephone"), request.getParameter("motdepasse"), false);
+			System.out.println(index);
+			user = new Utilisateur(index, 23, request.getParameter("pseudo"), request.getParameter("Nom"), request.getParameter("Prenom"),request.getParameter("email"),
+					request.getParameter("Telephone"), request.getParameter("Adresse"), request.getParameter("Cp"), request.getParameter("Ville"), request.getParameter("motdepasse"), false);
 			userdao.updateProfil(user);
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Profil.jsp");
 			rd.forward(request, response);
