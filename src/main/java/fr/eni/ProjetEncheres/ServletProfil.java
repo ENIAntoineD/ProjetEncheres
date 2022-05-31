3fb484a279660929fece0b4fe8b453b75452d90a
antoi <antoine.david2022@campus-eni.fr>
2022-05-31 10:26:22 +0200

    Profil utilisateur

---
+++ src/main/java/fr/eni/ProjetEncheres/ServletProfil.java
@@ -0,0 +1,43 @@
+package fr.eni.ProjetEncheres;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.ProjetEncheres.bo.Utilisateur;
+import fr.eni.ProjetEncheres.dal.jdbc.UtilisateurDAOJdbcImpl;
+
+
+/**
+ * Servlet implementation class ServletUtilisateur
+ */
+@WebServlet("/ServletUtilisateur")
+public class ServletProfil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int index = Integer.parseInt(request.getParameter("index"));
+		UtilisateurDAOJdbcImpl userdao = new UtilisateurDAOJdbcImpl();
+		Utilisateur user = null;
+		user = userdao.getid(index);
+		request.setAttribute("personne", user);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/utilisateur.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
